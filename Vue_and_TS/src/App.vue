213d678477abe1e9c85d06301241204c1c4ac76b
izaiasramos <izaiasramos123456789@gmail.com>
2025-04-08 22:26:09 -0300

    iniciando projeto

---
+++ Vue_and_TS/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="./assets/images/logo.svg" alt="Logo do Cookin' Up" 
+            class="logo"/>
+
+      <p class="cabecalho-lh frase-cabecalho">
+        <span class="texto-verde">Um banquete de ideias para </span>
+        despertar o chef que há em você!
+      </p>      
+
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+    </div>
+  </header>
+</template>
